8cc2e85f39128eed55ee67a272fcad6511bb6fdd
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-05-24 15:12:45 -0500

    Quick Commit

--- resources/views/supporters.blade.php
+++ resources/views/supporters.blade.php
@@ -1,24 +1,13 @@
    <section
       id="supporters"
-      class="supporters  post-15 page type-page status-publish hentry _off"
-      data-video-cover="/images/cameraman.mp4.jpg"
-      data-video-mp4="/videos/cameraman.mp4.mp4"
-      data-video-webm="/videos/cameraman.webmsd.webm"
-      data-video-ogg="/videos/cameraman.oggtheora.ogv">
+      class="supporters  post-15 page type-page status-publish hentry _off">
       <div class="supporters-container pb-pattern o-lines-light fs-background-element fs-background">
          <div class="container">
             <div class="row content-padding-lg">
                <div class="col-sm-12 text-center">
                   <h2 class="section-title">Supporters</h2>
                   <div class="row content-padding">
-                     <div id="supporters-masonry" style="position: relative; width: 1090px; height: 150px;">
-                        <div class="supporter" id="petition-8" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.&lt;/q&gt;&lt;cite&gt;Raymond Carter&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 0px; top: 0px;"> <img alt="" src="images//7feb7878694915ba2cd534b0c6466f41" srcset="http://1.gravatar.com/avatar/7feb7878694915ba2cd534b0c6466f41?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-11" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;Cum ceteris in veneratione tui montes, nascetur mus. Inmensae subtilitatis, obscuris et malesuada fames.&lt;/q&gt;&lt;cite&gt;Kimberly Howard&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 160px; top: 0px;"> <img alt="" src="images//982a9f59b6a2b4ab088633393b1f33df" srcset="http://0.gravatar.com/avatar/982a9f59b6a2b4ab088633393b1f33df?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-14" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.&lt;/q&gt;&lt;cite&gt;Phyllis Hall&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 320px; top: 0px;"> <img alt="" src="images//32212df9dce9637a711942939a7b0490" srcset="http://0.gravatar.com/avatar/32212df9dce9637a711942939a7b0490?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-40" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;Testando!&lt;/q&gt;&lt;cite&gt;Gustavo Leite&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 480px; top: 0px;"> <img alt="" src="images//ba636afcf1a94ca54918cb38b40ca8a7" srcset="http://2.gravatar.com/avatar/ba636afcf1a94ca54918cb38b40ca8a7?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-70" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;testing&lt;/q&gt;&lt;cite&gt;test test&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 640px; top: 0px;"> <img alt="" src="images//c7a66b449773a484d389c31bd1991a92" srcset="http://0.gravatar.com/avatar/c7a66b449773a484d389c31bd1991a92?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-53" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;hhjhk&lt;/q&gt;&lt;cite&gt;Sasg hte&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 800px; top: 0px;"> <img alt="" src="images//bf23697f1374cc52fb01eea9a88dcfbe" srcset="http://2.gravatar.com/avatar/bf23697f1374cc52fb01eea9a88dcfbe?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
-                        <div class="supporter" id="petition-69" data-container="body" data-toggle="popover" data-placement="auto" data-html="true" data-content="&lt;q&gt;weils weil !&lt;/q&gt;&lt;cite&gt;asdbhfjlg sadfbjkladsfasjdl&lt;/cite&gt;" data-original-title="" title="" style="position: absolute; left: 960px; top: 0px;"> <img alt="" src="images//77de1d409a25468417f8a11e94c158ae" srcset="http://1.gravatar.com/avatar/77de1d409a25468417f8a11e94c158ae?s=1024&amp;d=mm&amp;r=g 2x" class="avatar avatar-512 photo" height="512" width="512"></div>
+                     <div id="supporters-masonry">
                      </div>
                   </div>
                   <h3><strong class="counter" data-number="31">31</strong> Supporters Already. <small>Be one of them!</small></h3>
