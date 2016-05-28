1817528df818799fda19b26b8ab3d3f3ce2e3642
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-05-28 17:54:59 -0500

    Quick Commit

--- config/site.php
+++ config/site.php
@@ -5,5 +5,6 @@ return [
 	'email' => 'petition@diasporaladan.org',
 	'url' => 'http://diasporaladan.org',
 	'description' => "Pétition pour une révision constitutionnelle en faveur des haitiens vivant à l'étranger. par l'honorable député Jerry Tardieu",
-	'logo' => 'logo-500x500.jpg'
+	'logo' => 'logo-500x500.jpg',
+	'twitter' => 'diasporaladan'
 ];
