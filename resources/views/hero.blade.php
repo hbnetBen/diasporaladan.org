743772af9b760fe453b300923a14c187b1d7ffa1
Jean Gérard Bousiquot <jgb@JGB-MacBook-Pro.local>
2016-06-22 15:27:52 -0400

    Quick Commit

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -1,7 +1,8 @@
 <section
 	class="crossfade hero pb-pattern o-lines-light hentry" id="hero">
 	<div id="splash">
-	 	<h1>Pétition pour une révision constitutionnelle en faveur des haïtiens vivant à l'étranger.
+	 	<h1>Pétition pour une révision constitutionnelle devant permettre à la diaspora Haïtienne
+	 	 d'élir ses représentants au parlement Haïtien.
 	 		<small>par l'honorable député Jerry Tardieu</small></h1>
 		<p>
 			<a
